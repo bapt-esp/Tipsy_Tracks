00a38f2aa43729f731d4f10f30c8dbe61e75c6b3
Aliénor VABRE <alienor.vabre@epfedu.fr>
2025-03-10 15:37:11 +0100

    Suppression de niveaux inutiles et ajout du menu

--- phaser_base_3_niveaux/src/index.js
+++ phaser_base_3_niveaux/src/index.js
@@ -1,8 +1,6 @@
 // chargement des librairies
-import selection from "/src/js/selection.js";
-import niveau1 from "/src/js/niveau1.js";
-import niveau2 from "/src/js/niveau2.js";
-import niveau3 from "/src/js/niveau3.js";
+import menu from "/src/js/menu.js";
+
 
 // configuration générale du jeu
 var config = {
@@ -18,19 +16,16 @@ var config = {
     // définition des parametres physiques
     default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
     arcade: {
-      // parametres du mode arcade
-      gravity: {
-        y: 300 // gravité verticale : acceleration ddes corps en pixels par seconde
-      },
+
       debug: true // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
     }
   },
-  scene: [selection, niveau1, niveau2, niveau3]
+  scene: [menu]
 };
 
 // création et lancement du jeu
 var game = new Phaser.Game(config);
-game.scene.start("selection");
+game.scene.start("menu");
 
-var slime,
-//My name is Aliénor and I am 19 years old
+
+//My name is Aliénor and I am 19 years old,
---
+++ phaser_base_3_niveaux/src/js/menu.js
@@ -0,0 +1,61 @@
+import * as fct from "/src/js/fonctions.js";
+
+/***********************************************************************/
+/** VARIABLES GLOBALES 
+/***********************************************************************/
+
+
+// définition de la classe "selection"
+export default class selection extends Phaser.Scene {
+  constructor() {
+    super({ key: "menu" }); // mettre le meme nom que le nom de la classe
+  }
+
+  /***********************************************************************/
+  /** FONCTION PRELOAD 
+/***********************************************************************/
+
+  /** La fonction preload est appelée une et une seule fois,
+   * lors du chargement de la scene dans le jeu.
+   * On y trouve surtout le chargement des assets (images, son ..)
+   */
+  preload() {
+    // tous les assets du jeu sont placés dans le sous-répertoire src/assets/
+    this.load.image("img_ciel", "src/assets/sky.png")
+    this.load.image("img_bouton", "src/assets/Boutton.png")
+  
+  }
+
+  /***********************************************************************/
+  /** FONCTION CREATE 
+/***********************************************************************/
+
+  /* La fonction create est appelée lors du lancement de la scene
+   * si on relance la scene, elle sera appelée a nouveau
+   * on y trouve toutes les instructions permettant de créer la scene
+   * placement des peronnages, des sprites, des platesformes, création des animations
+   * ainsi que toutes les instructions permettant de planifier des evenements
+   */
+  create() {
+    this.background = this.add.tileSprite(400, 300, 800, 600, "img_ciel");
+
+    let playButton = this.add.image(400, 500, "img_bouton").setInteractive();
+    playButton.on("pointerdown", () => {
+        this.scene.start("GameScene");
+    });
+}
+  
+
+  /***********************************************************************/
+  /** FONCTION UPDATE 
+/***********************************************************************/
+
+update() {
+  this.background.tilePositionY -= 2;
+}
+}
+
+
+/***********************************************************************/
+/** CONFIGURATION GLOBALE DU JEU ET LANCEMENT 
+/***********************************************************************/
