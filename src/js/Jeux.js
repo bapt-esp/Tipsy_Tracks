05eed6a5d84b7d398fedb57734276ffe4cb4812a
baptiste espanol <baptiste.espanol@epfedu.fr>
2025-03-11 14:06:30 +0100

    tentative de mouvement du fond

--- src/js/Jeux.js
+++ src/js/Jeux.js
@@ -40,7 +40,7 @@ Elle crée le sol, le personnage, les obstacles et les animations.*/
 create() {
 /*A modifier à la fin si besoin. refaire le fond en 800x800
     avec une fenetre de 800x800 et une bande de terre de 600 de large*/
-    this.background = this.add.tileSprite(400,400, 400, 400, "img_background");
+    this.background = this.add.tileSprite(400,300, 800, 600, "img_background");
     this.background.setScale(3);
 
     this.perso = this.physics.add.sprite(this.positions[this.currentPositionIndex], 500, "img_perso");
@@ -82,7 +82,7 @@ create() {
 }
 
 
-update(time) {
+update() {
     
     this.background.tilePositionY -= 2;
 
