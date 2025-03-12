87225d0b3eec2ed4afebfca87cd82bdb9c20db8b
baptiste espanol <baptiste.espanol@epfedu.fr>
2025-03-12 14:53:05 +0100

    mise en place règle

--- src/js/Helps.js
+++ src/js/Helps.js
@@ -6,10 +6,17 @@ export default class help extends Phaser.Scene {
     
 preload(){
 this.load.image("img_regle", "src/assets/menu.png")
+this.load.image("img_quitte",'src/assets/boutton_quitter.png')
 }
 
 create(){
 this.regle = this.add.image(400, 300, "img_regle");
+
+let playButtonhelp = this.add.image(400, 600, "img_quitte").setInteractive();
+    playButtonhelp.setScale(2);
+    playButtonhelp.on("pointerdown", () => {
+        this.scene.start("menu");
+    });
 }
 
 update(){
