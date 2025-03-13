fc4e568e0f62b2e0aa208ec816533d7921facab4
Aliénor VABRE <alienor.vabre@epfedu.fr>
2025-03-13 20:50:38 +0100

    JSon de fin retiré

--- src/js/Jeux.js
+++ src/js/Jeux.js
@@ -53,7 +53,7 @@ pour la gestion du personnage et du gameplay.*/
 
     this.load.audio('son_piece', 'src/assets/piecesound.mp3');
     this.load.audio('son_bouteille', 'src/assets/bouteillesound.mp3');
-    this.load.audio('son_GameOver', 'src/assets/perdu.mp3')
+
 }
 
 /*La fonction create() initialise les objets du jeu après le chargement des ressources. 
@@ -314,7 +314,7 @@ create() {
     // Ajouter les effets sonores
     this.sonPiece = this.sound.add('son_piece');
     this.sonBouteille = this.sound.add('son_bouteille');
-    this.sonGameOver = this.sound.add('son_GameOver')
+
 }
 
 
@@ -680,9 +680,6 @@ gameOver() {
     this.background2.body.setVelocityY(0); // Arrête le mouvement du background
     this.background2.body.allowGravity = false; // Désactive la gravité
     
-    this.time.delayedCall(100, () => {
-        this.sonGameOver.play();
-    }, this);
 
     // Arrêter le défilement des rails
     this.rails.forEach(rail => {
